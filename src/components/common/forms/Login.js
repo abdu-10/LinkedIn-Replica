3dd98f08377a1d557f939c121be12f72b971f5c3
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-05-17 19:40:32 +0300

    updates Login page

--- src/components/common/forms/Login.js
+++ src/components/common/forms/Login.js
@@ -1,91 +1,63 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <div className="p-1 ml-[150px]">
-        {/* logo */}
-        <img
-          className="w-[150px]"
-          src="https://www.logo.wine/a/logo/LinkedIn/LinkedIn-Logo.wine.svg"
-          alt="logo"
-        />
-      </div>
+    <>
+      {/* <!-- component --> */}
+      <div class="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
+        {/* <!-- linkedin logo --> */}
+        <div class=" flex w-96 ">
+          <img
+            className="w-[150px]"
+            src="https://www.logo.wine/a/logo/LinkedIn/LinkedIn-Logo.wine.svg"
+            alt="logo"
+          />
+        </div>
+        <div className="text-xl  text-gray-800 dark:text-black md:text-2xl">
+          Make the most of your professional life
+        </div>
 
-      <div className="text-center">
-        <p className="text-[30px] ">Make the most of your professional life</p>
-      </div>
-
-      <div className="bg-white mx-auto rounded-md p-3 w-[400px] mb-10">
-        <form>
-          <div className="flex flex-col ">
-            <lable for="username" className="username text-[12px]">
-              username
-            </lable>
+        {/* <!-- Layout  --> */}
+        <div class="bg-white w-96 shadow-xl rounded p-5">
+          <h1 class="text-3xl font-medium">Sign In</h1>
+          <p class="text-sm">Stay updated on your professional world</p>
+          <form class="space-y-5 mt-5">
             <input
-              className="p-1 rounded-[3px] text-[18px] border focus:outline-black border-black"
-              placeholder="username"
+              type="text"
+              class="w-full h-12 border border-gray-800 rounded px-3"
+              placeholder="Username"
             />
-          </div>
-          <div className="flex flex-col  mt-[20px]">
-            <lable for="email" className="email text-[12px]">
-              email
-            </lable>
-            <input
-              type="email"
-              className="p-1 rounded-[3px] text-[18px] border focus:outline-black border-black"
-              placeholder="example@gmail.com"
-            />
-          </div>
-          <div className="flex flex-col  mt-[20px]">
-            <lable for="password" className="email text-[12px]">
-              Password (6 or more characters)
-            </lable>
             <input
               type="password"
-              className="p-1 rounded-[3px] text-[18px] border focus:outline-black border-black"
+              class="w-full h-12 border border-gray-800 rounded px-3"
+              placeholder="Password"
             />
-          </div>
-          <div className="flex flex-col space-y-2 mt-[20px]">
-            <lable for="profile image" className="email text-[12px]">
-              profile image
-            </lable>
-            <input type="file" accept="image/*" />
-          </div>
 
-          <div className="text-center text-[12px] -space-y-2">
-            <p>
-              By clicking Agree & Join, you agree to the LinkedIn{" "}
-              <span className="text-blue-600 cursor-pointer ">User</span>{" "}
-            </p>
-            <p>
-              <span className="text-blue-600  cursor-pointer  ">Agreement</span>
-              ,{" "}
-              <span className="text-blue-600  cursor-pointer ">
-                Privacy Policy
-              </span>
-              , and{" "}
-              <span className="text-blue-600 cursor-pointer ">
-                Cookie Policy
-              </span>
-              .
-            </p>
-          </div>
+            <div>
+              <Link
+                to="/signup"
+                class="font-medium text-blue-700 hover:bg-blue-300 rounded-full p-2"
+              >
+                Forgot password?
+              </Link>
+            </div>
 
-          <button className="w-[100%] rounded-full p-4 bg-blue-600 mt-3 text-white">
-            Agree and Join
-          </button>
-        </form>
-        <p className="text-center text-[12px]">
-          Already on LinkedIn?{" "}
-          <span className="text-blue-600 font-semibold cursor-pointer hover:underline">
-            Sign in
-          </span>
+            <button class="text-center w-full bg-blue-700 rounded-full text-white py-3 font-medium">
+              Sign Up
+            </button>
+          </form>
+        </div>
+
+        {/* <!-- Footer --> */}
+        <p>
+          New To LinkedIn?{" "}
+          <Link to="/signup" class="text-blue-700 font-medium">
+            Join Now
+          </Link>{" "}
         </p>
       </div>
-      {/* footer */}
-    </div>
+    </>
   );
 }
 
