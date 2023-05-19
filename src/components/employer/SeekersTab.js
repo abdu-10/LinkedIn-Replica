41c0ea165281ed0550024b5cf63046b1a23efca1
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-05-19 15:29:43 +0300

    modify styling on seekerstab

--- src/components/employer/SeekersTab.js
+++ src/components/employer/SeekersTab.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function SeekersTab() {
   return (
-    <div className='mt-40'>
-<ul class="hidden text-sm font-medium text-center text-black divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-blue-700 dark:text-blue-400">
+    <div className='mt-15 ml-5 w-1/3'>
+<ul class="hidden text-sm font-medium text-center text-black divide-gray-200 rounded-lg shadow sm:flex dark:divide-blue-700 dark:text-blue-400">
     <li class="w-1/2">
         <Link to="/employer/seekers" class="inline-block w-full p-4 text-blue-700 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-blue-700 dark:text-white" aria-current="page">All Seekers</Link>
     </li>
