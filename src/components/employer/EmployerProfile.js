f1a5567e0430e379911b72c88607badd96fe39d1
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-05-19 15:29:59 +0300

    adds employersprofile

---
+++ src/components/employer/EmployerProfile.js
@@ -0,0 +1,214 @@
+import React from "react";
+import EmployerNav from "../employer/EmployerNav";
+
+function EmployerProfile() {
+  return (
+    <>
+      <EmployerNav />
+      <div class=" mx-auto mt-50 my-5 p-5 ">
+        <div class="md:flex no-wrap md:-mx-2 mt-9 ">
+          {/* <!-- Left Side --> */}
+          <div class="w-full md:w-3/12 md:mx-2">
+            {/* <!-- Profile Card --> */}
+            <div class="bg-white p-3 border-t-4 border-green-400">
+              <div class="image overflow-hidden">
+                <img
+                  class="h-auto w-full mx-auto"
+                  src="https://www.logomaker.com/wpstatic/uploads/2021/10/LogoMaker.jpg"
+                  alt=""
+                />
+              </div>
+              <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">
+                Motos Ltd
+              </h1>
+              <h3 class="text-gray-600 font-lg text-semibold leading-6">
+                A technology company that builds innovative solutions for
+                regulated industries.
+              </h3>
+              <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur
+                non deserunt
+              </p>
+              <ul class="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+                <li class="flex items-center py-3">
+                  <span>Status</span>
+                  <span class="ml-auto">
+                    <span class="bg-green-500 py-1 px-2 rounded text-white text-sm">
+                      Active
+                    </span>
+                  </span>
+                </li>
+                <li class="flex items-center py-3">
+                  <span>Member since</span>
+                  <span class="ml-auto">Nov 07, 2016</span>
+                </li>
+              </ul>
+            </div>
+            {/* <!-- End of profile card --> */}
+            <div class="my-4"></div>
+            {/* <!-- Friends card --> */}
+            <div class="bg-white p-3 hover:shadow">
+              <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
+                <span class="text-green-500">
+                  <svg
+                    class="h-5 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                </span>
+                <span>Similar Profiles</span>
+              </div>
+              <div class="grid grid-cols-3">
+                <div class="text-center my-2">
+                  <img
+                    class="h-16 w-16 rounded-full mx-auto"
+                    src="https://cdn.australianageingagenda.com.au/wp-content/uploads/2015/06/28085920/Phil-Beckett-2-e1435107243361.jpg"
+                    alt=""
+                  />
+                  Kojstantin
+                </div>
+                <div class="text-center my-2">
+                  <img
+                    class="h-16 w-16 rounded-full mx-auto"
+                    src="https://avatars2.githubusercontent.com/u/24622175?s=60&amp;v=4"
+                    alt=""
+                  />
+                  James
+                </div>
+                <div class="text-center my-2">
+                  <img
+                    class="h-16 w-16 rounded-full mx-auto"
+                    src="https://img.freepik.com/premium-vector/businesswoman-avatar-cartoon-character-profile_18591-50143.jpg?w=2000"
+                    alt=""
+                  />
+                  Natie
+                </div>
+                <div class="text-center my-2">
+                  <img
+                    class="h-16 w-16 rounded-full mx-auto"
+                    src="https://img.freepik.com/premium-vector/businessman-character-avatar-icon_24877-8260.jpg?w=2000"
+                    alt=""
+                  />
+                  Casey
+                </div>
+              </div>
+            </div>
+            {/* <!-- End of friends card --> */}
+          </div>
+          {/* <!-- Right Side --> */}
+          <div class="w-full md:w-9/12 mx-2">
+            {/* <!-- Profile tab -->
+                <!-- About Section --> */}
+            <div class="bg-white p-3 shadow-sm rounded-sm">
+              <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+                <span clas="text-green-500">
+                  <svg
+                    class="h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
+                </span>
+                <span class="tracking-wide">Company Info</span>
+              </div>
+              <div class="text-gray-700">
+                <div class="grid md:grid-cols-2 text-sm">
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Company Name</div>
+                    <div class="px-4 py-2">Motos Ltd</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Location</div>
+                    <div class="px-4 py-2">pembe Road</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Phone Number</div>
+                    <div class="px-4 py-2">+11 998001001</div>
+                  </div>
+                  <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Email</div>
+                    <div class="px-4 py-2">motos@gmail.com</div>
+                  </div>
+                </div>
+              </div>
+              <button class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">
+                Edit Profile
+              </button>
+            </div>
+            {/* <!-- End of about section --> */}
+
+            <div class="my-4"></div>
+
+            {/* <!-- Experience and education --> */}
+            <div class="bg-white p-3 shadow-sm rounded-sm">
+              <div class="">
+                <div>
+                  <div class="flex items-center space-x-2 font-semibold text-gray-900 mb-3">
+                    <span clas="text-green-500">
+                      <svg
+                        class="h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                        />
+                      </svg>
+                    </span>
+                    <span class="tracking-wide">About</span>
+                  </div>
+                  <p>
+                    Pivot Access is the leading software and cloud
+                    infrastructure service provider in Rwanda and East Africa
+                    and has a proven track record of delivering exceptional
+                    software solutions that are customized to meet the unique
+                    needs of regulated institutions. With over 12 years of
+                    experience in digital security and a deep understanding of
+                    financial, commercial, and telecommunications digital
+                    environments, Pivot Access is dedicated to creating
+                    solutions that are centered around inclusion, reliability,
+                    and security. The company's product portfolio includes
+                    cutting-edge technologies such as prepaid vending systems,
+                    payment systems, systems integrations, banking applications,
+                    real-time transaction reconciliations, electronic merchant
+                    payments, traffic management solutions, and modern data
+                    center infrastructures. By leveraging its expertise and
+                    experience, Pivot Access is committed to empowering various
+                    industries to innovate while meeting compliance standards
+                    requirements.
+                  </p>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default EmployerProfile;
