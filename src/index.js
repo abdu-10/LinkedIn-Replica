9400ca8adda3bdbdfa539c42b51122dba3d943fd
steve <austines811@gmail.com>
2023-05-19 16:52:29 +0300

    wrap with redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import { persistor, store } from "./store/store"
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
