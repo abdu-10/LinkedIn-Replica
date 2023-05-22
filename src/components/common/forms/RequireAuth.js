b86898f2f2edcf7c39a09fa0bc84c3c9da67ea61
steve <austines811@gmail.com>
2023-05-22 22:11:42 +0300

    prerun component for route based auth

---
+++ src/components/common/forms/RequireAuth.js
@@ -0,0 +1,23 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentUserRole } from "../../../features/users/userSlice";
+
+
+const RequireAuth = ({ requiredRouteRole }) => {
+    const role = useSelector(selectCurrentUserRole)
+    const location = useLocation();
+
+    const hasPermissionToRoute = (requiredRouteRole) => {
+        return role === requiredRouteRole;
+    };
+
+    return (
+        hasPermissionToRoute(requiredRouteRole)
+            ? <Outlet />
+            : role
+                ? <Navigate to="/unauth" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
+    );
+}
+
+export default RequireAuth;
