bf18d21262a277e7ad269a6e1cfc46a143744585
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-05-23 09:21:41 +0300

    adds common apis

--- src/api/common/commonApis.js
+++ src/api/common/commonApis.js
@@ -25,3 +25,62 @@ export const generateToken = async (
         password,
     })
 }
+
+// create a Post
+export const createPost = async (
+    title,
+    media,
+    description,
+    user_code,
+    likes,
+) => {
+    return await apis.post(`/posts`, {
+        title,
+        media,
+        description,
+        user_code,
+        likes,
+    })
+}
+
+export const BrowsePosts = async () => {
+    return await apis.get(`/posts`)
+}
+
+export const getUserPost = async (user_code) => {
+    return await apis.get(`/posts/users/${user_code}`)
+}
+
+export const getIndividualPost = async (post_code) => {
+    return await apis.get(`/posts/${post_code}`)
+}
+
+export const deletePost = async (post_code) => {
+    return await apis.delete(`/posts/${post_code}`)
+}
+
+// Comment on post
+export const commentOnPost = async (
+    content,
+    post_code,
+    user_code,
+) => {
+    return await apis.post(`/comments`, {
+        content,
+        post_code,
+        user_code,
+    })
+}
+
+export const getAllPostComment = async (post_code) => {
+    return await apis.get(`/posts/comments/${post_code}`)
+}
+
+export const deleteComment = async (comment_code) => {
+    return await apis.delete(`/comments/${comment_code}`)
+}
+
+
+
+
+
