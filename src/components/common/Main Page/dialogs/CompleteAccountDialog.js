6832c9077212ec8911f8b918174a40d77936067e
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-05-31 14:42:46 +0300

    creates completeacc dialog

--- src/components/common/Main Page/dialogs/CompleteAccountDialog.js
+++ src/components/common/Main Page/dialogs/CompleteAccountDialog.js
@@ -1,136 +1,185 @@
 import React, { useState } from "react";
 import { Dialog } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectLoggedInUserRef } from "../../../../features/users/userSlice";
 import { completeSeekerAccount } from "../../../../api/seeker/seekerApis";
+import { setCurrentSeekerDetail } from "../../../../features/seekers/seekerSlice";
+import CustomSnackbar from "../../utils/CustomSnackbar";
+const CompleteAccountDialog = ({
+  openCompleteAccountDialog,
+  closeCompleteAccountDialog,
+}) => {
+  const user_code = useSelector(selectLoggedInUserRef);
+  console.log(user_code);
 
+  const dispatch = useDispatch();
 
-const CompleteAccountDialog = ({ openCompleteAccountDialog, closeCompleteAccountDialog }) => {
-    const user_code = useSelector(selectLoggedInUserRef)
-    console.log(user_code)
+  const [values, setValues] = useState({
+    full_name: "",
+    email: "",
+    phone_number: "",
+    gender: "",
 
-    const [values, setValues] = useState({
-        full_name: "",
-        email: "",
-        phone_number: "",
-        gender: "",
-    })
+    snackbarMessage: "",
+    openSnackbar: false,
+    snackbarSeverity: "success",
+  });
 
-    const {
-        full_name,
-        email,
-        phone_number,
-        gender,
-    } = values
+  const {
+    full_name,
+    email,
+    phone_number,
+    gender,
+    snackbarMessage,
+    openSnackbar,
+    snackbarSeverity,
+  } = values;
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        return completeSeekerAccount(full_name, phone_number, email, gender, user_code)
-        .then((res) => {
-            if (res.status === 201){
-                closeCompleteAccountDialog();
-                // success snackbar and close the daialog
-            }
-        })
-        .catch((err) => {
-            // log error and display error snackbar
-            console.log(err);
-            // setValues({
-            //   ...values,
-            //   snackbarMessage: err.message,
-            //   openSnackbar: true,
-            //   snackbarSeverity: "error",
-            // });
-          });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const closeSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
+    setValues({ ...values, openSnackbar: false });
+  };
 
-    return (
-        <Dialog
-            maxWidth="md"
-            fullWidth
-            open={openCompleteAccountDialog}
-            onClose={closeCompleteAccountDialog}
-        >
-            <div className="bg-white shadow-md rounded mt-20 px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
-                <div className="-mx-3 md:flex mb-6">
-                    <h2> Tell us a bit more about yourself</h2>
-                    <form className="flex" onSubmit={(e) => handleSubmit(e)}>
-                        <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label
-                                for="first_name"
-                                className="block text-xs font-semibold text-gray-600 uppercase"
-                            >
-                                Full Name
-                            </label>
-                            <input
-                                id="full_name"
-                                type="text"
-                                name="full_name"
-                                value={full_name}
-                                onChange={handleChange("full_name")}
-                                className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                            />
-                            <label
-                                for="first_name"
-                                className="block text-xs font-semibold text-gray-600 uppercase"
-                            >
-                                Email Address
-                            </label>
-                            <input
-                                id="email"
-                                type="text"
-                                name="email"
-                                value={email}
-                                onChange={handleChange("email")}
-                                className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                            />
-                            <label
-                                for="first_name"
-                                className="block text-xs font-semibold text-gray-600 uppercase"
-                            >
-                                Phone Number
-                            </label>
-                            <input
-                                id="full_name"
-                                type="number"
-                                name="full_name"
-                                value={phone_number}
-                                onChange={handleChange("phone_number")}
-                                className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                            />
-                            <label
-                                for="email"
-                                className="block text-xs font-semibold text-gray-600 uppercase"
-                            >
-                                Gender
-                            </label>
-                            <input
-                                id="email"
-                                type="text"
-                                name="gender"
-                                value={gender}
-                                onChange={handleChange("gender")}
-                                className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
-                            />
-                        </div>
-                        <div className="md:col-span-5 text-right">
-                            <div className="inline-flex items-end">
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded bottom mt-96" >
-                                    Add Details
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return completeSeekerAccount(
+      full_name,
+      phone_number,
+      email,
+      gender,
+      user_code
+    )
+      .then((res) => {
+        if (res.status === 201) {
+          // success snackbar and close the daialog
+          setValues({
+            ...values,
+            snackbarMessage: "Account Updated Succesfully",
+            openSnackbar: true,
+            snackbarSeverity: "success",
+          });
+          setTimeout(() => {
+            closeCompleteAccountDialog();
+          }, 3000);
+          dispatch(
+            setCurrentSeekerDetail({ currentSeekerDetail: res.data })
+          );
+        }
+      })
+      .catch((err) => {
+        // log error and display error snackbar
+        console.log(err);
+        setValues({
+          ...values,
+          snackbarMessage: err.message,
+          openSnackbar: true,
+          snackbarSeverity: "error",
+        });
+      });
+  };
+
+  return (
+    <Dialog
+      maxWidth="md"
+      fullWidth
+      open={openCompleteAccountDialog}
+      onClose={closeCompleteAccountDialog}
+    >
+      <CustomSnackbar
+        openSnackbar={openSnackbar}
+        handleClose={closeSnackbar}
+        snackbarMessage={snackbarMessage}
+        snackbarSeverity={snackbarSeverity}
+      />
+      <div class="w-full bg-grey-500">
+        <div class="container mx-auto py-8">
+          <div class="w-96 mx-auto bg-white rounded shadow">
+            <div class="mx-1 py-4 px-8 text-black text-xl font-bold border-b border-grey-500">
+              Tell us a bit more about yourself
             </div>
 
-        </Dialog>
-    )
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              name="student_application"
+              id="student_application"
+              action=""
+            >
+              <div class="py-4 px-8">
+                <div class="mb-4">
+                  <label class="block text-grey-darker text-sm font-bold mb-2">
+                    Full Name
+                  </label>
+                  <input
+                    class=" border rounded w-full py-2 px-3 text-grey-darker"
+                    type="text"
+                    id="full_name"
+                    name="full_name"
+                    value={full_name}
+                    onChange={handleChange("full_name")}
+                    placeholder="Enter Your Full Name"
+                  />
+                </div>
 
-}
+                <div class="mb-4">
+                  <label class="block text-grey-darker text-sm font-bold mb-2">
+                    Email Address
+                  </label>
+                  <input
+                    class=" border rounded w-full py-2 px-3 text-grey-darker"
+                    id="email"
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={handleChange("email")}
+                    placeholder="Enter Your Email"
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label class="block text-grey-darker text-sm font-bold mb-2">
+                    Phone Number
+                  </label>
+                  <input
+                    class=" border rounded w-full py-2 px-3 text-grey-darker"
+                    id="full_name"
+                    type="number"
+                    name="full_name"
+                    value={phone_number}
+                    onChange={handleChange("phone_number")}
+                    placeholder="Enter Your Phone Number"
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label class="block text-grey-darker text-sm font-bold mb-2">
+                    Gender
+                  </label>
+                  <input
+                    class=" border rounded w-full py-2 px-3 text-grey-darker"
+                    id="gender"
+                    type="text"
+                    name="gender"
+                    value={gender}
+                    onChange={handleChange("gender")}
+                  />
+                </div>
+                <div class="mb-4">
+                  <button class="mb-2 mx-16 rounded-full py-1 px-24 bg-gradient-to-r from-blue-600 to-blue-700 ">
+                    Add Details
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
 
 export default CompleteAccountDialog;
