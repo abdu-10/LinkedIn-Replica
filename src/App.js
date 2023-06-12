78e0e47d0faeb6b9aa8564d7dc98d372b4e55e89
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-06-12 15:43:08 +0300

    adds routes for jobtags components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Maincontent from "./components/common/Main Page/Maincontent";
+import MainContent from "./components/common/MainPage/MainContent";
 import Login from "../src/components/common/forms/Login";
 import SignIn from "./components/common/forms/SignIn";
 import Register from "./components/common/forms/Register";
 import Profile from "./components/seeker/Profile";
-import Landing from "../src/components/common/Main Page/Landing";
+import Landing from "./components/common/MainPage/Landing";
 import JobsPage from "./components/seeker/JobsPage";
 import AdminDashboard from "./components/admin/AdminDashboard";
 import AllEmployers from "./components/admin/views/AllEmployers";
@@ -14,8 +14,8 @@ import AllSeekers from "./components/admin/views/AllSeekers";
 import JobPosting from "./components/employer/JobPosting";
 import JobApplication from "./components/seeker/JobApplication";
 import MyNetwork from "./components/seeker/MyNetwork";
-import Messages from "./components/common/Main Page/Messages";
-import Notification from "./components/common/Main Page/Notification";
+import Messages from "./components/common/MainPage/Messages";
+import Notification from "./components/common/MainPage/Notification";
 import AdminNav from "./components/admin/Navs/AdminNav";
 import EmployerNav from "./components/employer/EmployerNav";
 import UsersNav from "./components/admin/Navs/UsersNav";
@@ -31,6 +31,8 @@ import CreateAdmin from "./components/admin/CreateAdmin";
 import UnAuthorized from "./components/common/views/UnAuthorized";
 import RequireAuth from "./components/common/forms/RequireAuth";
 import EditSeekerDetails from "./components/seeker/EditSeekerDetails";
+import ConfigJobs from "./components/admin/ConfigJobs";
+import CreateJobs from "./components/admin/CreateJobs";
 
 function App() {
   return (
@@ -49,9 +51,17 @@ function App() {
         {/* <Route element={<RequireAuth requiredRouteRole={"ADMIN"} />}> */}
         <Route path="/admin" element={<AdminNav />}>
           <Route index element={<AdminDashboard />} />
-          <Route path="employers" element={<AllEmployers />} />
-          <Route path="seekers" element={<AllSeekers />} />
+          <Route path="employers">
+            <Route index element={<AllEmployers/>} />
+            <Route path="details" element={<EmployerDetails />} />
+          </Route>
+          <Route path="seekers">
+          <Route index element={<AllSeekers />} />
+          <Route path="details" element={<SeekerDetails />} />
+          </Route>
           <Route path="createadmin" element={<CreateAdmin />} />
+          <Route path="configjob" element={<ConfigJobs />} />
+          <Route path="createjob" element={<CreateJobs />} />
           {/* TO VERIFY USER: Open pop up */}
           {/* <Route path="verify" element={<Verify />} /> */}
           {/* element={<UsersNav />} */}
@@ -66,35 +76,30 @@ function App() {
 
         {/* EMPLOYER ROUTES */}
         {/* <Route element={<RequireAuth requiredRouteRole={"EMPLOYER"} />}> */}
-          <Route path="/employer" element={<EmployerNav />}>
-            <Route index element={<Maincontent />} />
-            <Route path="dashboard" element={<Maincontent />} />
-            <Route path="job-post" element={<JobPosting />} />
-            {/* PENDING: FIND LOGIC TO FIND SEEKERS WHILE DISPLAYING AN INITIAL CURATED LIST */}
-            <Route path="seekers" element={<EmployerSpecific />} />
-            <Route path="myseekers" element={<MySeekers />} />
-            <Route path="profile" element={<EmployerProfile />} />
-            <Route path="details" element={<EmployerDetails />} />
-          </Route>
+        <Route path="/employer" element={<EmployerNav />}>
+          <Route index element={<MainContent />} />
+          <Route path="dashboard" element={<MainContent />} />
+          <Route path="job-post" element={<JobPosting />} />
+          {/* PENDING: FIND LOGIC TO FIND SEEKERS WHILE DISPLAYING AN INITIAL CURATED LIST */}
+          <Route path="seekers" element={<EmployerSpecific />} />
+          <Route path="myseekers" element={<MySeekers />} />
+          <Route path="profile" element={<EmployerProfile />} />
+        </Route>
         {/* </Route> */}
         {/* SEEKER ROUTES */}
         {/* <Route element={<RequireAuth requiredRouteRole={"SEEKER"} />}> */}
-          <Route path="/seeker" element={<Navbar />}>
-            <Route index={true} element={<Maincontent />} />
-            <Route path="jobs" element={<JobsPage />}>
-              <Route path="apply" exact element={<JobApplication />} />
-            </Route>
-            <Route path="network" element={<MyNetwork />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="details" element={<SeekerDetails />} />
+        <Route path="/seeker" element={<Navbar />}>
+          <Route index={true} element={<MainContent />} />
+          <Route path="jobs" element={<JobsPage />}>
+            <Route path="apply" exact element={<JobApplication />} />
           </Route>
+          <Route path="network" element={<MyNetwork />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         {/* </Route> */}
-        <Route path="/edit" element={<EditSeekerDetails />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-// npm install @mui/material @emotion/react @emotion/styled
-//npm install @mui/icons-material
