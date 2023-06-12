8c4ebc3c2e5cf02a1c770da137182c6dbb7da43f
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-06-12 15:40:21 +0300

    adds job config tab

---
+++ src/components/admin/Navs/Jobs.Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function JobsNav() {
+  return (
+    <div>
+        <div className='mt-20 ml-5 w-1/3'>
+<ul class="hidden text-sm font-medium text-center text-black divide-gray-200 rounded-lg shadow sm:flex dark:divide-blue-700 dark:text-blue-400">
+    <li class="w-1/2">
+        <Link to="/admin/configjob" class="inline-block w-full p-4 text-blue-700 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-blue-700 dark:text-white" aria-current="page">Configured Jobs </Link>
+    </li>
+    <li class="w-1/2">
+        <Link to="/admin/createjob" class="inline-block w-full p-4 bg-white hover:text-black hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-white dark:hover:bg-blue-700">Create Job Tag</Link>
+    </li>
+</ul>
+
+    </div>
+    </div>
+  )
+}
+
+export default JobsNav
