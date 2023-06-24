170886552c569a39f51ea101e34048deb4662634
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-06-24 08:53:07 +0300

    clean up

--- src/components/seeker/Profile.js
+++ src/components/seeker/Profile.js
@@ -66,7 +66,7 @@ function Profile() {
               <div class="image overflow-hidden">
                 <img
                   class="h-auto w-full mx-auto"
-                  src={seekerDetails.avatar}
+                  src={seekerDetails.avatar_url}
                   alt=""
                 />
               </div>
@@ -96,63 +96,7 @@ function Profile() {
             </div>
             {/* <!-- End of profile card --> */}
             <div class="my-4"></div>
-            {/* <!-- Friends card --> */}
-            <div class="bg-white p-3 hover:shadow">
-              <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
-                <span class="text-green-500">
-                  <svg
-                    class="h-5 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-                    />
-                  </svg>
-                </span>
-                <span>Similar Profiles</span>
-              </div>
-              <div class="grid grid-cols-3">
-                <div class="text-center my-2">
-                  <img
-                    class="h-16 w-16 rounded-full mx-auto"
-                    src="https://cdn.australianageingagenda.com.au/wp-content/uploads/2015/06/28085920/Phil-Beckett-2-e1435107243361.jpg"
-                    alt=""
-                  />
-                  Kojstantin
-                </div>
-                <div class="text-center my-2">
-                  <img
-                    class="h-16 w-16 rounded-full mx-auto"
-                    src="https://avatars2.githubusercontent.com/u/24622175?s=60&amp;v=4"
-                    alt=""
-                  />
-                  James
-                </div>
-                <div class="text-center my-2">
-                  <img
-                    class="h-16 w-16 rounded-full mx-auto"
-                    src="https://img.freepik.com/premium-vector/businesswoman-avatar-cartoon-character-profile_18591-50143.jpg?w=2000"
-                    alt=""
-                  />
-                  Natie
-                </div>
-                <div class="text-center my-2">
-                  <img
-                    class="h-16 w-16 rounded-full mx-auto"
-                    src="https://img.freepik.com/premium-vector/businessman-character-avatar-icon_24877-8260.jpg?w=2000"
-                    alt=""
-                  />
-                  Casey
-                </div>
-              </div>
-            </div>
-            {/* <!-- End of friends card --> */}
+          
           </div>
           {/* <!-- Right Side --> */}
           <div class="w-full md:w-9/12 mx-2">
@@ -229,87 +173,7 @@ function Profile() {
 
             <div class="my-4"></div>
 
-            {/* <!-- Experience and education --> */}
-            <div class="bg-white p-3 shadow-sm rounded-sm">
-              <div class="grid grid-cols-2">
-                <div>
-                  <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
-                    <span clas="text-green-500">
-                      <svg
-                        class="h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                        />
-                      </svg>
-                    </span>
-                    <span class="tracking-wide">Experience</span>
-                  </div>
-                  <ul class="list-inside space-y-2">
-                    <li>
-                      <div class="text-teal-600">Owner at Her Company Inc.</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Owner at Her Company Inc.</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Owner at Her Company Inc.</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Owner at Her Company Inc.</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
-                    <span clas="text-green-500">
-                      <svg
-                        class="h-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z" />
-                        <path
-                          fill="#fff"
-                          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                        />
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                        />
-                      </svg>
-                    </span>
-                    <span class="tracking-wide">Education</span>
-                  </div>
-                  <ul class="list-inside space-y-2">
-                    <li>
-                      <div class="text-teal-600">Masters Degree in Oxford</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                    <li>
-                      <div class="text-teal-600">Bachelors Degreen in LPU</div>
-                      <div class="text-gray-500 text-xs">March 2020 - Now</div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              {/* <!-- End of Experience and education grid --> */}
-            </div>
+            
             {/* <!-- End of profile tab --> */}
           </div>
         </div>
