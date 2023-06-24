b3d5f3c6fbac1c417904c71c50c92ec198f40a21
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-06-24 08:44:00 +0300

    updates apis with updated backend

--- src/api/admin/adminApis.js
+++ src/api/admin/adminApis.js
@@ -4,12 +4,12 @@ import { apis } from "../axios";
 export const completeAdminAccount = async (
     full_name,
     email,
-    user_code,
+    user_id,
 ) => {
     return await apis.post(`/admins`, {
         full_name,
         email,
-        user_code,
+        user_id,
     })
 }
 
@@ -17,39 +17,39 @@ export const getAllAdmins = async () => {
     return await apis.get(`/admins`)
 }
 
-export const getIndividualAdmin = async (admin_code) => {
-    return await apis.get(`/admins/${admin_code}`)
+export const getIndividualAdmin = async (id) => {
+    return await apis.get(`/admins/${id}`)
 }
 
 export const getAllEmployers = async () => {
     return await apis.get(`/employers`)
 }
 
-export const getIndividualEmployer = async (employer_code) => {
-    return await apis.get(`/employers/${employer_code}`)
+export const getIndividualEmployer = async (id) => {
+    return await apis.get(`/employers/${id}`)
 }
 
 export const getAllSeekers = async () => {
     return await apis.get(`/seekers`)
 }
 
-export const verifyEmployer = async (employer_code) => {
-    return await apis.patch(`/employers/${employer_code}/verify`, {
+export const verifyEmployer = async (id) => {
+    return await apis.patch(`/employers/${id}/verify`, {
         verified: true
     })
 }
 
-export const verifySeeker = async (seeker_code) => {
-    return await apis.patch(`/seekers/${seeker_code}/verify`, {
+export const verifySeeker = async (id) => {
+    return await apis.patch(`/seekers/${id}/verify`, {
         verified: true
     })
 }
 
-export const deleteUser = async (user_code) => {
-    return await apis.delete(`/user/${user_code}`)
+export const deleteUser = async (user_id) => {
+    return await apis.delete(`/user/${user_id}`)
 }
-export const getAdminProfile = async (user_ref) => {
-    return await apis.get(`/admin/${user_ref}/profile`);
+export const getAdminProfile = async (id) => {
+    return await apis.get(`/admin/${id}/profile`);
   };
 
   export const configJob = async (
